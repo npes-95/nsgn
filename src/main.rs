e9125bd2f0c0f760085d153866a2a9fbf860b116
npes-95 <npe.smith.95@gmail.com>
2020-07-20 17:42:38 +0200

    move cli config to yaml file

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,15 @@
+#[macro_use]
 extern crate clap;
-
-use std::env;
+use clap::App;
 use std::process;
-
-use clap::{Arg, App};
 
 fn main() {
 
-    let config = App::new(env!("CARGO_PKG_NAME"))
-        .version(env!("CARGO_PKG_VERSION"))
-        .author(env!("CARGO_PKG_AUTHORS"))
-        .about(env!("CARGO_PKG_DESCRIPTION"))
-        .arg(Arg::with_name("Color")
-                .short('c')
-                .long("color")
-                .takes_value(true)
-                .about("Noise color: white, pink, brownian, blue, violet, grey."))
-        .arg(Arg::with_name("Interpolation")
-                .short('i')
-                .long("interpolation")
-                .takes_value(true)
-                .about("Interpolation type: linear, polynomial, spline."))
-        .arg(Arg::with_name("Length")
-                .short('l')
-                .long("length")
-                .takes_value(true)
-                .about("Length of generated audio in seconds."))
-        .arg(Arg::with_name("Output file")
-                .short('o')
-                .long("output-file")
-                .takes_value(true)
-                .about("Output file: [filename.wav]"))        
-        .arg(Arg::with_name("Sample Rate")
-                .short('s')
-                .long("sample-rate")
-                .takes_value(true)
-                .about("Sample rate: 16k, 44.1k, 48k"))
-        .get_matches();
+    let yaml = load_yaml!("../cli.yml");
+    let config = App::from_yaml(yaml).get_matches();
     
     if let Err(e) = nsgn::run(config) {
         eprintln!("Application error: {}", e);
         process::exit(1);
     } 
 }
-
